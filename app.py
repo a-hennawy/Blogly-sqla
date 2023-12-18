4449495332ba5f4c9d86ff421ff9019424d60184
Ahmed <a.hennawy97@hotmail.com>
2023-12-18 23:10:33 +0200

    final commit

--- app.py
+++ app.py
@@ -1,12 +1,80 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template, request, flash
+from models import connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mac@localhost:5432/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret'
 
-connect_db(app)
+
+db = connect_db(app)
 db.create_all()
+
+
+@app.route("/")
+def root_redirect():
+    return redirect("/users")
+
+
+@app.route("/users")
+def user_list():
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template("user-list.html", users=users)
+
+
+@app.route("/users/new")
+def get_new_user_form():
+    return render_template("add-user.html")
+
+
+@app.route("/users/new", methods=['POST'])
+def add_new_user():
+    f_name = request.form["first-name"]
+    l_name = request.form["last-name"]
+    image_url = request.form["image-url"]
+
+    new_user = User(first_name=f_name, last_name=l_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect("/users")
+
+
+@app.route("/users/<int:user_id>")
+def get_user_by_id(user_id):
+    user = User.query.get_or_404(user_id)
+
+    return render_template("user-detail.html", user=user)
+
+
+@app.route("/users/<int:user_id>", methods=["POST"])
+def POST_get_user_by_id(user_id):
+    f_name = request.form["first-name"]
+    l_name = request.form["last-name"]
+    image_url = request.form["image-url"]
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = f_name
+    user.last_name = l_name
+    user.image_url = image_url
+
+    db.session.add(user)
+    db.session.commit()
+
+    return render_template("user-detail.html", user=user)
+
+
+@app.route("/users/<int:user_id>/edit")
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template("edit-user.html", user=user)
+
+
+@app.route("/users/<int:user_id>/delete")
+def delete_user(user_id):
+    User.query.filter_by(id=user_id).delete()
+    db.session.commit()
+
+    return redirect("/users")
--- models.py
+++ models.py
@@ -1,1 +1,44 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+
+def connect_db(app):
+    db.init_app(app)
+    app.app_context().push()
+    return db
+# Model class
+
+
+class User(db.Model):
+
+    def __repr__(self):
+        """Shows user information"""
+        u = self
+        return f"<User: id={u.id}, name={u.first_name + ' '+u.last_name}>"
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                   autoincrement=True,
+                   primary_key=True)
+
+    first_name = db.Column(db.String(15),
+                           nullable=False)
+
+    last_name = db.Column(db.String(15),
+                          nullable=False)
+
+    image_url = db.Column(db.String,
+                          nullable=False,
+                          default=DEFAULT_IMAGE_URL)
+
+    # def get_full_name(self):
+    #     return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
